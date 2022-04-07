6710ecce49b752ee553811ba7951b513f288fa5b
Rihards Slūtiņš <rikartc1@gmail.com>
2022-04-07 18:09:35 +0300

    created a simple express server, as well as added a few files for certain dev-dependencies to work properly

---
+++ server/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import cors from 'cors';
+
+// creates an express app
+const app = express();
+
+// runs express app on the PORT variable or 3000
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server running on port: ${process.env.PORT || 3000}`);
+});
+
+// MIDDLEWARE
+
+// allows cross site resource sharing
+app.use(cors());
+// parses data coming into the server into json
+app.use(express.json);
