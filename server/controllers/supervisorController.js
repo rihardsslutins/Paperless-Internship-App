45ebb7b353a9471771901faa57eaaa3432578769
Rihards Slūtiņš <rikartc1@gmail.com>
2022-05-13 13:41:51 +0300

    handled custom error object sent from the server in the client

--- server/controllers/supervisorController.js
+++ server/controllers/supervisorController.js
@@ -2,30 +2,13 @@
 import Supervisor from '../models/supervisor.js';
 
 const handleErrors = (err) => {
-
-  const errorsCompanyAddress = { 
-    country: '',
-    city: '',
-    zipCode: '',
-    streetName: '',
-    streetNumber: '',
-}
-
-  const company = {
-    name: '',
-    address: errorsCompanyAddress,
-    registrationNumber: '',
-    email: '',
-    phone: '',
-  }
-
   const errors = {
     name: '',
     surname: '',
     gender: '',
     phone: '',
     field: '',
-    company: company,
+    company: '',
     email: '',
     password: '',
   };
@@ -34,19 +17,11 @@ const handleErrors = (err) => {
     errors.email = 'E-pasts jau ir reģistrēts';
     return errors;
   }
-
   Object.values(err.errors).forEach(({ properties }) => {
-    if (!properties.path.includes('company')) {
-      errors[properties.path] = properties.message;
-    }
-    if (properties.path.includes('company') && !properties.path.includes('company.address')) {
-      company[properties.path.slice(8)] = properties.message;
-    }
-    if (properties.path.includes('company.address.')) {
-      errorsCompanyAddress[properties.path.slice(16)] = properties.message;
-    }
+    errors[properties.path] = properties.message;
   });
-  return errors
+
+  return errors;
 };
 
 const supervisor_create = async (req, res) => {
@@ -56,7 +31,7 @@ const supervisor_create = async (req, res) => {
     res.status(201).json(supervisor);
   } catch (err) {
     const errors = handleErrors(err);
-    res.status(400).json({errors});
+    res.status(400).json({ errors });
   }
 };
 
